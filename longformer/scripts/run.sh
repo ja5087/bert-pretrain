0ebb6caa0e484c1b5ec2ec862793414a5a15d8ef
Ja (Thanakul) Wattanawong <j.wat@eecs.berkeley.edu>
2022-09-16 00:37:32 -0700

    Adjust batch size for 8x24GB

--- longformer/scripts/run.sh
+++ longformer/scripts/run.sh
@@ -4,7 +4,7 @@ pretrain.py \
 --save_dir /scratch/ja/roberta_save/ \
 --input_dir /dnn/j.wat/cache_roberta_tokenizer/ \
 --gpu_count 8 \
---grad_accum 4 \
---batch_size 32 \
+--grad_accum 8 \
+--batch_size 16 \
 --num_workers 8 \
 --fp16 True 
